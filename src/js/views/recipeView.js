8df2268ab8ee7bd8531a6f5dd9e1eee1fae67922
skarfroth <mikko.raussi1@gmail.com>
2022-12-02 10:41:24 +0200

    Even more fixes to fractional

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,6 @@
 import View from './View.js';
 import icons from 'url:../../img/icons.svg';
-const fracty = require('fracty');
+import { numberToFraction } from '../helpers.js';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -128,7 +128,7 @@ class RecipeView extends View {
         <use href="${icons}#icon-check"></use>
       </svg>
       <div class="recipe__quantity">${
-        ing.quantity ? fracty(ing.quantity).toString() : ''
+        ing.quantity ? numberToFraction(ing.quantity).toString() : ''
       }</div>
       <div class="recipe__description">
         <span class="recipe__unit">${ing.unit}</span>
