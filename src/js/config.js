79060e5fe2c3ee80efa9dda091ac8b4144569b80
skarfroth <mikko.raussi1@gmail.com>
2022-12-02 09:21:42 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '76671368-04b2-4faf-a6a3-4dc5b3af5c23';
+export const MODAL_CLOSE_SEC = 2.5;
